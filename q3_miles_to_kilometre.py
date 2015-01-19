1aad9edcd4fad1ab2e87a7801f97e9793bf46809
Lee Yang Peng <lee.yangpeng@dhs.sg>
2015-01-19 17:24:07 +0800

    q3 and q4

---
+++ q3_miles_to_kilometre.py
@@ -0,0 +1,7 @@
+__author__ = 'dhs'
+
+miles = float(input("Give me a mile and I'll convert to kilometre"))
+
+kilometres = miles * 1.60934
+
+print("This is the number of kilometres (3 d.p):", round(kilometres,3))
