a62de7f2c8f84112d2e23c1c2b7b03d2261bf232
Lee Yang Peng <lee.yangpeng@dhs.sg>
2015-02-04 08:26:34 +0800

    phonebook IO

---
+++ exercises/exercise_5b_phonebook.py
@@ -0,0 +1,60 @@
+__author__ = 'dhs'
+
+def contacts(book):
+    try:
+        outfile = open("CONTACTS.DAT", "w")
+        for k,v in book.items():
+            outfile.write("{0:20.20s} {1:.8}\n".format(k, v))
+    except IOError:
+        print("Can't write to Contacts")
+    return 0
+
+def newcontacts(book):
+    try:
+        outfile = open("NEWCONTACTS.DAT", "a")
+        for k,v in book.items():
+            outfile.write("{0:20s} {1:8}\n".format(k, v))
+    except IOError:
+        print("Can't write to New Contacts")
+    return 0
+
+x = 0
+phonebook = {}
+while x == 0:
+    command = input("Input your command! Search/Insert/Update/Delete/Display/Quit: ")
+    if command == "Search":
+        entry = input("Input a name to search for: ")
+        if entry in phonebook:
+            print(phonebook[entry])
+        else:
+            print("Entry not found!")
+    elif command == "Insert":
+        name = input("What is the name? ")
+        if name not in phonebook:
+            number = input("What is the phone number? ")
+            phonebook[name] = number
+        else:
+            print("The name is already in the phonebook!")
+    elif command == "Update":
+        name = input("What is the name? ")
+        if name in phonebook:
+            number = input("What is the phone number? ")
+            phonebook[name] = number
+        else:
+            print("The name is not in the phonebook!")
+    elif command == "Delete":
+        name = input("What is the name? ")
+        if name in phonebook:
+            del phonebook[name]
+        else:
+            print("The name is not in the phonebook!")
+    elif command == "Display":
+        for k,v in phonebook.items():
+            print(k,v)
+    elif command == "Quit":
+        print("Thank you for using Lim Ah Seng Phonebook!")
+        contacts(phonebook)
+        newcontacts(phonebook)
+        x = 1
+    else:
+        print("Please input a valid command! With Capital letters! Search/Insert/Update/Delete/Display/Quit: ")
